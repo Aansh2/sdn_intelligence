7e9ac5fc682f998ef8df02a1a7dda1d10fb890c3
Fernando Benayas <f.benayas@alumnos.upm.es>
2018-04-13 10:02:26 +0200

    Added authors

--- mininet/errors.py
+++ mininet/errors.py
@@ -8,10 +8,43 @@ import json
 import time
 import os
 
-"""
-Python script with methods used to introduce errors within the network
+# -*- coding: utf-8 -*-
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#
+# Copyright (c) 2018  Fernando Benayas  <ferbenayas94@gmail.com>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#title           : errors.py
+#date created    : 20/02/2018
+#python_version  : 3.5.1
+#notes           :
+__author__ = "Fernando Benayas"
+__license__ = "GPLv2"
+__version__ = "0.1.0"
+__maintainer__ = "Fernando Benayas"
+__email__ = "ferbenayas94@gmail.com"
 
-__author__ = "GSI"
+"""This program can change the license header inside files.
+"""
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
+"""
+Errors module
+
+Takes on the communication with Opendaylight to introduce errors, and logs each error
 """
 
 
--- mininet/errors_balanced.py
+++ mininet/errors_balanced.py
@@ -7,10 +7,42 @@ import logging
 import json
 import time
 import os
+
+# -*- coding: utf-8 -*-
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#
+# Copyright (c) 2018  Fernando Benayas  <ferbenayas94@gmail.com>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#title           : errors_balanced.py
+#date created    : 20/02/2018
+#python_version  : 3.5.1
+#notes           :
+__author__ = "Fernando Benayas"
+__license__ = "GPLv2"
+__version__ = "0.1.0"
+__maintainer__ = "Fernando Benayas"
+__email__ = "ferbenayas94@gmail.com"
+
+"""This program can change the license header inside files.
+"""
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
 """
 ***TEST SCRIPT*** 
-
-__author__ = "GSI"
 """
 
 error_dictionary = {1: {'err_type': '1', 'Desc': 'A host is doing heavy use of the network by requiring a lot of streaming traffic ', 'Params': {'Host': '', 'Timestamp': ''}}}
